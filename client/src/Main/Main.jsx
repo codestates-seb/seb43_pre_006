ac3a901ba8470f9f946636ae1a2d171c7fea9c1a
GiRang Choi <choigirang@kakao.com>
2023-04-15 23:19:48 +0900

    feat: Main 컴포넌트 중간 저장
    SIdeBar 컴포넌트 추가

--- client/src/Main/Main.jsx
+++ client/src/Main/Main.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import SideBar from "./SideBar/SideBar";
 
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <Container>
+      <SideBar></SideBar>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  width: calc(100% - 164px);
+  max-width: 1100px;
+  padding: calc(24px * 1);
+`;
