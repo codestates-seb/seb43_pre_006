7f84718f23333688cc32b88e686ca724ce45cbd4
GiRang Choi <choigirang@kakao.com>
2023-04-15 23:18:21 +0900

    feat: 글로벌 테마 설정
    ThemeProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,38 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import Main from "./Main/Main";
+import styled, { ThemeProvider } from "styled-components";
+import theme from "./style/theme";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Head></Head>
+        <Bottom>
+          <Main></Main>
+        </Bottom>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Head = styled.div`
+  width: 100%;
+  height: 50px;
+  background-color: #fbc6c6;
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
