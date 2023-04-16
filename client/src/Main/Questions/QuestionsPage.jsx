3bd7d3ded9a44f5890e18a47f86536bc1f803afd
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-04-16 16:25:56 +0900

    feat: Question(Main) 작성
    구조 잡기

--- client/src/Main/Questions/QuestionsPage.jsx
+++ client/src/Main/Questions/QuestionsPage.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import QuestionsTop from "./Top/QuestionsTop";
+import styled from "styled-components";
+import QuestionsBottom from "./Bottom/QuestionsBottom";
 
 export default function QuestionsPage() {
-  return <div>QuestionsPage</div>;
+  return (
+    <Container>
+      <QuestionsTop />
+      <QuestionsBottom />
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: calc(100% - 300px - 24px);
+`;
