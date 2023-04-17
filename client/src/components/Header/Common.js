819b6872b865da36fcaa64bf164fc4386f4aa583
GiRang Choi <choigirang@kakao.com>
2023-04-17 13:42:29 +0900

    feat: 이경찬 merge

---
+++ client/src/components/Header/Common.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import Main from "./../Main/Main";
+import Nav from "./../NavBar/Nav";
+
+function Common() {
+  const CommonPage = styled.div`
+    width: 100%;
+    padding-top: 50px;
+    display: flex;
+    justify-content: center;
+  `;
+
+  return (
+    <>
+      <CommonPage>
+        <Nav></Nav>
+        <Main />
+      </CommonPage>
+    </>
+  );
+}
+
+export default Common;
