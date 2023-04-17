1452f1a799e27510add45246542c633eace3738f
GiRang Choi <choigirang@kakao.com>
2023-04-17 23:28:39 +0900

    feat: API 데이터 예시 작성

---
+++ client/src/components/Main/Questions/Bottom/ExampleList.jsx
@@ -0,0 +1,34 @@
+export const List = {
+  data: [
+    {
+      questionId: 2,
+      title: "제목입니다2",
+      content: "안에 있는 내용입니다.2",
+      createdAt: "2023-01-02T14:33:31.517606",
+      modifiedAt: "2023-01-02T14:33:31.517606",
+      score: 0,
+      viewCount: 0,
+      userName: "kimcodingding",
+      userEmail: "kcdd@gmail.com",
+      answers: [],
+    },
+    {
+      questionId: 1,
+      title: "제목입니다",
+      content: "안에 있는 내용입니다.",
+      createdAt: "2023-01-02T14:33:10.7882",
+      modifiedAt: "2023-01-02T14:33:10.7882",
+      score: 0,
+      viewCount: 1,
+      userName: "kimcodingding",
+      userEmail: "kcdd@gmail.com",
+      answers: [],
+    },
+  ],
+  pageInfo: {
+    page: 1,
+    size: 10,
+    totalElements: 2,
+    totalPages: 1,
+  },
+};
