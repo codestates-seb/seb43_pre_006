0c88bbaa7fd836534baf73f3cb7e635780aeae71
GiRang Choi <choigirang@kakao.com>
2023-04-19 15:02:36 +0900

    feat: AskPage 컴포넌트

--- client/src/components/Main/Questions/AskPage.jsx
+++ client/src/components/Main/Questions/AskPage.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 import styled from "styled-components";
+import AskPageTop from "./AskPageTop";
+import AskPageBottom from "./AskPageBottom";
 
 export default function AskPage() {
-  return <Container>AskPage</Container>;
+  return (
+    <Background>
+      <Container>
+        <AskPageTop />
+        <AskPageBottom />
+      </Container>
+    </Background>
+  );
 }
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   -webkit-box-pack: center;
   justify-content: center;
   margin-top: 50px;
-  width: calc(100% - 164px);
-  max-width: 1100px;
-  padding: calc(24px);
+  width: 100%;
+  max-width: 1264px !important;
+  flex: 1 0 auto;
+`;
+
+const Background = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  background: ${({ theme }) => theme.black050};
 `;
