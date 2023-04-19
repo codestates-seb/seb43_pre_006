ffd049ed63656a992cd89c370a6f1de2673f6029
GiRang Choi <choigirang@kakao.com>
2023-04-19 09:27:31 +0900

    feat: AskPage 컴포넌트 생성

---
+++ client/src/components/Main/Questions/AskPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function AskPage() {
+  return <Container>AskPage</Container>;
+}
+
+const Container = styled.div`
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+  margin-top: 50px;
+  width: calc(100% - 164px);
+  max-width: 1100px;
+  padding: calc(24px);
+`;
