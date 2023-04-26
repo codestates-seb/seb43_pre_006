761864e15580c3538721508fe5767a2604634fe1
GiRang Choi <choigirang@kakao.com>
2023-04-26 13:26:11 +0900

    feat: 텍스트 에디터 컴포넌트 작성

--- client/src/components/Main/Questions/ToastEditor.jsx
+++ client/src/components/Main/Questions/ToastEditor.jsx
@@ -4,21 +4,45 @@ import { useRef } from "react";
 import { Editor } from "@toast-ui/react-editor";
 import "@toast-ui/editor/dist/toastui-editor.css";
 import styled from "styled-components";
+import axios from "axios";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { textEditorState, titleText } from "../../store/atom";
 
 export default function ToastEditor() {
+  const [title, setTitle] = useRecoilState(titleText);
+  const [allowEditor, setAllowEditor] = useRecoilState(textEditorState);
+  console.log(allowEditor);
   // Editor DOM 선택용
   const editorRef = useRef();
 
   // 등록 버튼 핸들러
   const handleRegisterButton = () => {
-    // 입력창에 입력한 내용을 HTML 태그 형태로 취득
-    console.log(editorRef.current?.getInstance().getHTML());
-    // 입력창에 입력한 내용을 MarkDown 형태로 취득
-    console.log(editorRef.current?.getInstance().getMarkdown());
+    const cookie = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("user="));
+    if (cookie) {
+      const userId = cookie.split("=")[1];
+      axios
+        .post(`https://2c02-124-111-225-247.ngrok-free.app/questions/`, {
+          title: title,
+          content: editorRef.current?.getInstance().getMarkdown(),
+        })
+        .then((response) => {
+          console.log("Score updated successfully");
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+      setAllowEditor(!allowEditor);
+      setTitle("");
+    } else {
+      console.log("User is not logged in");
+      window.location.href = "http://localhost:3000/login";
+    }
   };
 
   return (
-    <Container>
+    <Container cursorAllow={allowEditor}>
       <Editor
         ref={editorRef} // DOM 선택용 useRef
         placeholder="내용을 입력해주세요."
@@ -33,16 +57,26 @@ export default function ToastEditor() {
           ["table", "image", "link"],
           ["code", "codeblock"],
         ]}
-        useCommandShortcut={false} // 키보드 입력 컨트롤 방지
+        useCommandShortcut={false}
+        style={{
+          // Editor 컴포넌트에 style 속성 추가
+          cursor: allowEditor ? "auto" : "not-allowed", // allowEditor 값에 따라 cursor 값 설정
+          opacity: allowEditor ? 1 : 0.3, // allowEditor 값에 따라 opacity 값 설정
+        }}
+        disabled={!allowEditor} // 키보드 입력 컨트롤 방지
       ></Editor>
 
-      <BtnStyle onClick={handleRegisterButton}>Post Your Answer</BtnStyle>
+      <BtnStyle onClick={handleRegisterButton} cursorAllow={allowEditor}>
+        Post Your Answer
+      </BtnStyle>
     </Container>
   );
 }
 
 const Container = styled.div`
   margin-top: 16px;
+  cursor: ${({ cursorAllow }) => !cursorAllow && "not-allowed"} !important;
+  opacity: ${({ cursorAllow }) => !cursorAllow && 0.5} !important;
 `;
 
 const BtnStyle = styled.button`
@@ -56,7 +90,8 @@ const BtnStyle = styled.button`
   box-shadow: inset 0 1px 0 0 hsla(0, 0%, 100%, 0.4);
   font-weight: 400;
   border: solid 1px transparent;
-  cursor: pointer;
+  cursor: ${({ cursorAllow }) => (!cursorAllow ? "not-allowed" : "pointer")};
+
   margin-bottom: 12px;
   &:hover {
     background-color: ${({ theme }) => theme.blue600};
